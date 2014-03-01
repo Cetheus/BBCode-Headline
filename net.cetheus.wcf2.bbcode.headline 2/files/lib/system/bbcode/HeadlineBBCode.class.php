292c7661b1f59e952272095eb6f8c15eea2ba5c9
Cetheus <Cetheus@gmx.net>
2014-03-01 01:37:30 +0100

    Version 1.0.1

--- net.cetheus.wcf2.bbcode.headline 2/files/lib/system/bbcode/HeadlineBBCode.class.php
+++ net.cetheus.wcf2.bbcode.headline 2/files/lib/system/bbcode/HeadlineBBCode.class.php
@@ -4,7 +4,7 @@ use wcf\system\WCF;
 
 /**
  * Parses the [headline] bbcode tag.
- *
+ * 
  * @author	Sascha Schickberger
  * @copyright	2014 Cetheus.net
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -17,9 +17,13 @@ class HeadlineBBCode extends AbstractBBCode {
 	 * @see	\wcf\system\bbcode\IBBCode::getParsedTag()
 	 */
 	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
-
+		
 		if ($parser->getOutputType() == 'text/html') {
-			return ('<h4 class="headlineBBCode">'.$content.'</h4>');
+			if(isset($openingTag['attributes'][0])){
+				return ('<h4 class="headlineBBCode" id="h-'.$openingTag['attributes'][0].'"><a href="'. WCF::getAnchor('h-'.$openingTag['attributes'][0]) .'">'.$content.'</a></h4>');
+			} else {
+				return ('<h4 class="headlineBBCode">'.$content.'</h4>');
+			}
 		}
 		if ($parser->getOutputType() == 'text/simplified-html') {
 			return ("----".$content."----\n");
